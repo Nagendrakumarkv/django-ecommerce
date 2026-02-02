e20a2ca3894349c790eb5d4816a8893c486c8869
Nagendra Kumar K V <nagendrakumarkv123@gmail.com>
2026-02-02 11:53:31 +0530

    Day 2: The Catalog (Models). Create Category and Product models. Handle product images (Pillow library) and prices.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    cat_image = models.ImageField(upload_to='photos/categories', blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_url(self):
+        # We will use this later to link to the category page
+        return reverse('products_by_category', args=[self.slug])
+
+    def __str__(self):
+        return self.category_name
+    
+class Product(models.Model):
+    product_name    = models.CharField(max_length=200, unique=True)
+    slug            = models.SlugField(max_length=200, unique=True)
+    description     = models.TextField(max_length=500, blank=True)
+    price           = models.IntegerField()
+    images          = models.ImageField(upload_to='photos/products')
+    stock           = models.IntegerField()
+    is_available    = models.BooleanField(default=True)
+
+    # The Link: Delete product if category is deleted (CASCADE)
+    category        = models.ForeignKey(Category, on_delete=models.CASCADE) 
+
+    created_date    = models.DateTimeField(auto_now_add=True)
+    modified_date   = models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        # Link to product detail page
+        return reverse('product_detail', args=[self.category.slug, self.slug])
+
+    def __str__(self):
+        return self.product_name    
