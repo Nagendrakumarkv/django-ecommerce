732697a77e263a655ae8f51f1c7308d9515831f5
Nagendra Kumar K V <nagendrakumarkv123@gmail.com>
2026-02-11 19:54:08 +0530

    Day 7: The Cart Page. Build the UI to show the summary, total price calculation, and "Proceed to Checkout" button.
    Day 8: Checkout Forms. Create forms for Shipping Address and Billing info.

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
+from orders.forms import OrderForm
 
 # Private function to get the session key (the "Cart ID")
 def _cart_id(request):
@@ -60,5 +61,55 @@ def remove_cart_item(request, product_id):
     return redirect('cart')
 
 def cart(request, total=0, quantity=0, cart_items=None):
-    # This logic will be fleshed out in Day 7
-    return render(request, 'store/cart.html')
+    try:
+        tax = 0
+        grand_total = 0
+        # 1. Get the cart using the session ID
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        # 2. Get active items
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+
+        # 3. Loop to calculate totals
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+
+        # 4. Calculate Tax (e.g., 2%)
+        tax = (2 * total) / 100
+        grand_total = total + tax
+
+    except ObjectDoesNotExist:
+        pass # Just ignore if cart is empty
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
+    }
+    return render(request, 'store/cart.html', context)
+
+def checkout(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+
+        tax = (2 * total) / 100 # Assuming 2% tax
+        grand_total = total + tax
+
+    except ObjectDoesNotExist:
+        pass # Just ignore if cart is empty
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
+        'form': OrderForm(), # <--- Pass the empty form here
+    }
+    return render(request, 'store/checkout.html', context)
